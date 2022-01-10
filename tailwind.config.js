4a82cf54db801e16a977f21660504f059117d7b9
Fnwrkio <firmansyah.assignment@gmail.com>
2022-01-10 14:50:51 +0700

    feat: Assets, Metadata, and CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: [
+    // Use *.tsx if using TypeScript
+    './pages/**/*.js',
+    './components/**/*.js'
+  ]
+}
